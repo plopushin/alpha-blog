08f3c02bdd9a2b945468bceabc0f2117c14f665b
lopushin <paullopushinsky@gmail.com>
2016-12-22 17:42:05 +0000

    add association between user and add debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       #do something
       flash[:success] = "Aritlce was created"
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   #ensures that there is a title.
   #be sure to reload!
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 3, maximum: 300}
+  validates :user_id, presence: true
 end 

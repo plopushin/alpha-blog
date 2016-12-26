5b3570bfa38be209c2e26b517a63a30bd8deb4d1
lopushin <paullopushinsky@gmail.com>
2016-12-26 22:31:42 +0000

    Fixed list categories test to proper class name

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 #for integration tests
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   
   def setup
     @category = Category.create(name: "books")
@@ -11,7 +11,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   test "should show categories listing" do
     get categories_path
     assert_template 'categories/index'
-    #ensure link is to this
+    #ensure link is to this, and that they are the same?
     assert_select "a[href=?]", category_path(@category), text: @category.name
     assert_select "a{href=?]", category_path(@category2), text: @category2.name
   end

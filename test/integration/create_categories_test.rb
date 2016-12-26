2e0383ec447d5fb749348f8f6b2e1231ad7af072
lopushin <paullopushinsky@gmail.com>
2016-12-26 22:56:08 +0000

    Add ability for only admins to add category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 #for integration tests
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  # set user as an admin
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)  
+  end
   
   test "get new category form and create category" do
+    #simulate user logging in by creating a method, and need to add password
+    #method made in test_helper file, which all tests can use.
+    sign_in_as(@user, "password")
+    
     get new_category_path
     assert_template 'categories/new'
     #ensure that another category has been added
@@ -16,6 +24,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     #ensure that another category has been added
